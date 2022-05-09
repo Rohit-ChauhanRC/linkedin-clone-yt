edff3bbce09dd7c0a880375ac482814eafbf164c
ROHIT CHAUHAN <rc672800@gmail.com>
2022-05-09 17:53:16 +0530

    feed work in progress

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,19 @@
 import styles from "./Feed.module.css";
 import React from "react";
+import CreateIcon from "@mui/icons-material/Create";
 
 const Feed = () => {
   return (
-    <div>
-      <h1>Feed</h1>
+    <div className={styles.feed}>
+      <div classNAme={styles.feed__inputContainer}>
+        <div classNAme={styles.feed__input}>
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
