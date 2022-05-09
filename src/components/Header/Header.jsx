fb1a6e50a34472ee867d55a4a25fa5b7e4347269
ROHIT CHAUHAN <rc672800@gmail.com>
2022-05-09 11:46:55 +0530

    add avatar home options

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import styles from './Header.module.css';
-import SearchIcon from '@mui/icons-material/Search';
-import HeaderOption from './HeaderOption/HeaderOption';
-import HomeIcon from '@mui/icons-material/Home';
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
-
+import React from "react";
+import styles from "./Header.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption/HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatIcon from "@mui/icons-material/Chat";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 
 const Header = () => {
   return (
-    <div  className={styles.header}>
+    <div className={styles.header}>
+      <div className={styles.header__left}>
+        <img src="/images/linkedin.png" alt="logo" className={styles.logo} />
 
-      <div  className={styles.header__left}>
-        <img src="/images/linkedin.png" alt="logo" className={styles.logo}/>
-
-        <div
-        className={styles.header__search}>
-          <SearchIcon/>
+        <div className={styles.header__search}>
+          <SearchIcon />
           <input type="text" />
         </div>
       </div>
       <div className={styles.header__right}>
-        <HeaderOption title={'Home'} Icon={HomeIcon} />
-        <HeaderOption title={'My Network'} Icon={SupervisorAccountIcon } />
+        <HeaderOption title={"Home"} Icon={HomeIcon} />
+        <HeaderOption title={"My Network"} Icon={SupervisorAccountIcon} />
+        <HeaderOption title={"Jobs"} Icon={BusinessCenterIcon} />
+        <HeaderOption title={"Messaging"} Icon={ChatIcon} />
+        <HeaderOption title={"Notification"} Icon={NotificationsIcon} />
+        <HeaderOption
+          title={"Me"}
+          avatar="https://lh3.googleusercontent.com/ogw/ADea4I659tQorDiRhAmUtwE1Cmf5hLO6DampCqXvTOXNGw=s32-c-mo"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Header/HeaderOption/HeaderOption.jsx
+++ src/components/Header/HeaderOption/HeaderOption.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import styles from './HeaderOption.module.css';
+import { Avatar } from "@mui/material";
+import React from "react";
+import styles from "./HeaderOption.module.css";
 
-
-const HeaderOption = ({Icon, title}) => {
+const HeaderOption = ({ avatar, Icon, title }) => {
   return (
     <div className={styles.headerOption}>
       {Icon && <Icon className={styles.headerOption__icon} />}
-      <h3 className={styles.headerOption__title}>{ title}</h3>
+      {avatar && <Avatar className={styles.headerOption__icon} src={avatar} />}
+      <h3 className={styles.headerOption__title}>{title}</h3>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderOption
+export default HeaderOption;
